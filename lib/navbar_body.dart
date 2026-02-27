e31a04f43a4fbeb387495fcb9f2fabaeb0e34a96
Rizal-web969 <mrizalarifin217@gmail.com>
2026-02-27 14:37:52 +0700

    diajarin bang tamam

---
+++ lib/navbar_body.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/Services/Theme.dart';
+
+class NavbarBody extends StatelessWidget {
+  final String title;
+  final int index;
+
+  const NavbarBody({super.key, required this.title, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 10,
+      
+      
+      
+    );
+  }
+}
