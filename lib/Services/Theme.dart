9cc9e015639891715150694b76bd1eb2296ced0b
Rizal-web969 <mrizalarifin217@gmail.com>
2026-02-27 08:09:47 +0700

    Add text box, send button, header

---
+++ lib/Services/Theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class warna {
+  static Hitam() => Colors.black;
+  static Putih() => Colors.white;
+  static Hijau() => Color(0xFF25D366);
+  // static Hitam() => Colors.black;
+}
--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:whatsapp_clone/widgets/BottomNavBar.dart';
+import 'package:whatsapp_clone/Services/Theme.dart';
 
 class home extends StatefulWidget {
   const home({super.key});
@@ -149,9 +150,4 @@ class PanggilanPage extends StatelessWidget {
   }
 }
 
-class warna {
-  static Hitam() => Colors.black;
-  static Putih() => Colors.white;
-  static Hijau() => Color(0xFF25D366);
-  // static Hitam() => Colors.black;
-}
+
