98718e57ccf8fd048f51e5fc18ea82a602f918bb
Rizal-web969 <mrizalarifin217@gmail.com>
2026-03-01 22:17:05 +0700

    Update route_handler.dart

--- lib/Services/route_handler.dart
+++ lib/Services/route_handler.dart
@@ -12,7 +12,7 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       final title = args['title'] as String;
       final index = args['index'] as int;
       return MaterialPageRoute(builder: (context) => Chatpage(title: title, index: index));
-      return MaterialPageRoute(builder: (context) => const home());
+      // return MaterialPageRoute(builder: (context) => const home());
 
     case '/splashScreen':
       return MaterialPageRoute(builder: (context) => SplashScreen());
