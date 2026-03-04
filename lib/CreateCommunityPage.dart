466d5636ce6bdd019df3e0d104615df196d8f39b
Yanto <yanto01.tech@gmail.com>
2026-03-04 15:18:28 +0700

    createappbar

--- lib/CreateCommunityPage.dart
+++ lib/CreateCommunityPage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/Services/Theme.dart';
 import 'package:whatsapp_clone/CommunityPage.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:whatsapp_clone/PengaturanPage.dart';
 
 class CreateCommunity extends StatelessWidget {
   const CreateCommunity({super.key});
@@ -8,6 +10,28 @@ class CreateCommunity extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: warna.Putih(),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.chevron_left,
+              size: 19,
+              color: warna.Hitam(),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+        title: Text(
+          'Komunitas Baru',
+            style: TextStyle(
+            color: warna.Hitam(),
+            fontSize: 19,
+            ),
+        ),
+      ),
       body: Center(
         child: Text('Buat Comunity'),
       ),
