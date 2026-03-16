4b36f635c1c3aa0baaac5d79fd7a5d25f74f8f21
AlatBekam <muhammadhisyam845@gmail.com>
2026-03-16 11:17:19 +0700

    update theme

--- lib/Services/Theme.dart
+++ lib/Services/Theme.dart
@@ -9,7 +9,34 @@ class warna {
   static HijauTua() => Color.fromARGB(255, 13, 85, 39);
   static Transparan() => Colors.transparent;
   static AbuAbu() => Color.fromARGB(255, 200, 200, 200);
-  static AbuAbuTua() =>  Color.fromARGB(255, 98, 97, 97);
+  static AbuAbuTua() => Color.fromARGB(255, 98, 97, 97);
   static Merah() => const Color.fromARGB(255, 255, 89, 78);
   // static Hitam() => Colors.black;
+}
+
+class AppTheme {
+  static light() {
+    return ThemeData(
+      colorScheme: ColorScheme(
+        brightness: Brightness.light,
+        primary: Color(value),
+        onPrimary: onPrimary,
+        secondary: secondary,
+        onSecondary: onSecondary,
+        error: error,
+        onError: onError,
+        surface: surface,
+        onSurface: onSurface,
+      ),
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(),
+
+      textTheme: TextTheme(bodyLarge: TextStyle()),
+      listTileTheme: ListTileThemeData(),
+      inputDecorationTheme: InputDecorationTheme(),
+    );
+  }
+
+  static dark() {
+    return ThemeData();
+  }
 }
